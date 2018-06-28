0dc2074cc596e34495bc42b90e48bdd24adc6ca0
suelywu <suely9511@gmail.com>
2018-06-27 21:08:14 -0300

    Update MapsActivity.java

--- app/src/main/java/com/example/easyrecipes/easyrecipes/MapsActivity.java
+++ app/src/main/java/com/example/easyrecipes/easyrecipes/MapsActivity.java
@@ -1,4 +1,4 @@
-package com.example.easyrecipes.easyrecipes;
+package com.example.navdrawersample.googlemaps;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -23,24 +23,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    
+    
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng fatecSjc = new LatLng(-23.16213900679389,  -45.79529100000002);
+        mMap.addMarker(new MarkerOptions().position(fatecSjc).title("Marker in Fatec SJC"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(fatecSjc));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(fatecSjc, 18));
+
     }
 }
